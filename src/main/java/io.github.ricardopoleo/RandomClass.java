0cbb4c566c329f118b1ba0a1b7dec12969bcb9fd
RicardoPoleo <ricardo.poleo@cl.abstracta.us>
2021-06-24 17:11:21 -0400

    Added configurations to make automatic releases MvnCentral

---
+++ src/main/java/io.github.ricardopoleo/RandomClass.java
@@ -0,0 +1,22 @@
+package io.github.ricardopoleo;
+
+public class RandomClass {
+
+  private int value;
+
+  public RandomClass(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Just a random class");
+  }
+}
